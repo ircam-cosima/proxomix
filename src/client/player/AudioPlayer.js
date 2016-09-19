a17f61d653007cbf3421773d7f9404d5677b2f8c
Norbert Schnell <Norbert.Schnell@ircam.fr>
2016-09-19 17:46:08 +0200

    removed forgotten line

--- src/client/player/AudioPlayer.js
+++ src/client/player/AudioPlayer.js
@@ -82,7 +82,6 @@ export default class AudioPlayer {
     this.scheduler = scheduler;
     this.buffers = buffers;
 
-<<<<<<< HEAD
     this.localTrack = new LoopTrack();
     this.localTrack.connect(audioContext.destination);
     this.remoteTracks = new Map();
